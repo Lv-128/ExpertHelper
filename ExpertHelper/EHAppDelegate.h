b356911dc261365500708a165c816f889d3783d4
skatolyk <katolykus@mail.ru>
2014-10-31 14:10:02 +0200

    Added new storyboard

---
+++ ExpertHelper/EHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EHAppDelegate.h
+//  ExpertHelper
+//
+//  Created by Katolyk S. on 10/31/14.
+//  Copyright (c) 2014 Katolyk S. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
--- ExpertHelperTests/ExpertHelperTests.m
+++ ExpertHelperTests/ExpertHelperTests.m
@@ -2,11 +2,10 @@
 //  ExpertHelperTests.m
 //  ExpertHelperTests
 //
-//  Created by Andrii Mamchur on 10/29/14.
-//  Copyright (c) 2014 Andrii Mamchur. All rights reserved.
+//  Created by Katolyk S. on 10/31/14.
+//  Copyright (c) 2014 Katolyk S. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 
 @interface ExpertHelperTests : XCTestCase
@@ -15,26 +14,21 @@
 
 @implementation ExpertHelperTests
 
-- (void)setUp {
+- (void)setUp
+{
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
-- (void)tearDown {
+- (void)tearDown
+{
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
 }
 
 @end
