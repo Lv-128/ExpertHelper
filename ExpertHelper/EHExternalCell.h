ea52623d32c3e11bbf2e04a4bf53a3611a3f483f
olenapianyh <elena.pyanyh@qmail.com>
2014-11-10 10:42:49 +0200

    Added forms and controllers with prefixes

---
+++ ExpertHelper/EHExternalCell.h
@@ -0,0 +1,13 @@
+//
+//  CLExternalCell.h
+//  Interview Assistant
+//
+//  Created by nvlizlo on 30.10.14.
+//  Copyright (c) 2014 Katolyk S. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EHExternalCell : UITableViewCell
+
+@end
---
+++ ExpertHelper/EHExternalViewController.h
@@ -0,0 +1,13 @@
+//
+//  CLExternalViewController.h
+//  firstCalendarFrom
+//
+//  Created by alena on 10/30/14.
+//  Copyright (c) 2014 alenka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EHExternalViewController : UIViewController
+
+@end
