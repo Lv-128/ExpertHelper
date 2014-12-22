22ca99b0b7ada9d9fc75fd250af09a37e2e4ec87
OShymanskyi <alexandr7717@gmail.com>
2014-12-22 17:13:08 +0200

    added comment to shared strings

--- ExpertHelper/EHExternalViewController.m
+++ ExpertHelper/EHExternalViewController.m
@@ -71,13 +71,12 @@
 {
     [super viewDidLoad];
     NSLog(@"%@", NSHomeDirectory());
-<<<<<<< HEAD
 
 
-=======
+
     [self.tableView registerNib:[UINib nibWithNibName:@"EHGeneralInfoCell" bundle:nil]
          forCellReuseIdentifier:@"GeneralInfo"];
->>>>>>> FETCH_HEAD
+
     self.navigationItem.title = [NSString stringWithFormat:@"%@ %@", _interview.idExternal.idCandidate.firstName, _interview.idExternal.idCandidate.lastName];
     
     self.cellDateFormatter = [[NSDateFormatter alloc] init];
@@ -512,6 +511,8 @@
     [self parsFunc];
     [self unzip];
     
+    [self insertIntoExclesSharedString];
+    
     //----------------------------------- start parsing part inside action -------------------------------
     NSError *error;
     
@@ -671,19 +672,8 @@
     }
     [za CloseZipFile2];
 }
+int uniqueCountIndex;
 
-@end
-
-
-
-
-
-
-
-
-
-
-<<<<<<< HEAD
 - (void) insertIntoExclesSharedString {
     NSError *error;
     
@@ -692,11 +682,13 @@
     filePath1 = [filePath1 stringByAppendingPathComponent:@"xl"];
     filePath1 = [filePath1 stringByAppendingPathComponent:@"sharedStrings.xml"];
     NSMutableString* xml = [[NSMutableString alloc] initWithString:[NSMutableString stringWithContentsOfFile:filePath1 encoding:NSUTF8StringEncoding error:&error]];
-    int uniqueCountIndex = 295;
+    uniqueCountIndex = 295;
     
     for (int i = 0; i < _comment.count; i++) {
-        if (![_comment[i] isEqualToString:@""]) {
-            uniqueCountIndex++;
+        for (int j = 0; j < [_comment[i] count]; j++) {
+            if (![_comment[i][j] isEqualToString:@""]) {
+                uniqueCountIndex++;
+            }
         }
     }
     
@@ -719,25 +711,26 @@
         if ([ss isEqualToString:stringForComparing]) {
             k = i;
             
-            [xml insertString:@"<si><t>Strong</t></si>" atIndex:k];
-            [xml insertString:@"<si><t>Good</t></si>" atIndex:k];
-            [xml insertString:@"<si><t>Beginner</t></si>" atIndex:k];
-            [xml insertString:@"<si><t>None</t></si>" atIndex:k];
-            for (int i = 0; i < _comment.count; i++) {
-                if (![_comment[i] isEqualToString:@""]) {
-                    NSString *s =@"<si><t>";
-                    [s stringByAppendingString:_comment[i]];
-                    [s stringByAppendingString:@"</t></si>"];
-                    
-                    [xml insertString:s atIndex:k];
+        //    [xml insertString:@"<si><t>Strong</t></si>" atIndex:k];
+         //   [xml insertString:@"<si><t>Good</t></si>" atIndex:k];
+         //   [xml insertString:@"<si><t>Beginner</t></si>" atIndex:k];
+         //   [xml insertString:@"<si><t>None</t></si>" atIndex:k];
+            for (int ii = 0; ii < _comment.count; ii++) {
+                for (int j = 0; j < [_comment[ii] count]; j++) {
+                    if (![_comment[ii][j] isEqualToString:@""]) {
+                        NSString *s =@"<si><t>";
+                        s = [s stringByAppendingString:_comment[ii][j]];
+                        s = [s stringByAppendingString:@"</t></si>"];
+                        
+                        [xml insertString:s atIndex:k];
+                    }
                 }
             }
-=======
+            break;
+        }
+    }
+    
+    [xml writeToFile:filePath1 atomically:YES encoding:NSUTF8StringEncoding error:&error];
 
->>>>>>> FETCH_HEAD
-
-
-
-
-
-
+}
+@end
